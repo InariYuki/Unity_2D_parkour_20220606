fd306df408353442cbd12e5183e1cfa3a1040ed4
InariYuki <105775231+InariYuki@users.noreply.github.com>
2022-06-13 12:00:48 +0800

    Add Script

---
+++ Unity_2D_parkour_20220606/Assets/Scripts/SystemRun.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+namespace KitsuneYuki
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class SystemRun : MonoBehaviour
+    {
+        #region 資料
+        [SerializeField , Tooltip("跑步速度") , Range(0 , 5f)] private float run_speed = 3.5f;
+        [SerializeField , Tooltip("跳躍高度") , Range(0 , 1000)] private float jump_height = 350;
+        private Animator anim_ctl;
+        private Rigidbody2D body_ctl;
+        #endregion
+
+        #region 複雜功能
+        #endregion
+
+        #region 方法
+        private void Start()
+        {
+            print("Hello");
+        }
+        #endregion
+    }
+}
