663c4072b082c1c1502f4080f33e9d59e0096bcf
InariYuki <105775231+InariYuki@users.noreply.github.com>
2022-06-20 10:11:58 +0800

    Static tweak

--- Unity_2D_parkour_20220606/Assets/Scripts/StaticAPI.cs
+++ Unity_2D_parkour_20220606/Assets/Scripts/StaticAPI.cs
@@ -15,20 +15,24 @@ namespace KitsuneYuki
             print($"PI is {Mathf.PI}");
             Cursor.visible = false;*/
 
-            print(Camera.allCamerasCount);
-            print(Application.platform);
+            print("攝影機數量 = " + Camera.allCamerasCount);
+            print("目前平台 = " + Application.platform);
             Physics.sleepThreshold = 10;
+            print("睡眠臨界值 = " + Physics.sleepThreshold);
             Time.timeScale = 0.5f;
-            print(Mathf.FloorToInt(9.999f));
-            print(Vector3.Magnitude(new Vector3(1, 1, 1) - new Vector3(22, 22, 22)));
+            print("時間大小 = " + Time.timeScale);
+            print("四捨五入的9.999是" + Mathf.Round(9.999f));
+            print("無條件捨去的9.999是" + Mathf.Floor(9.999f));
+            print("無條件進位的9.999是" + Mathf.Ceil(9.999f));
+            print("(1 , 1 , 1)跟(22 , 22 , 22)的距離是" + Vector3.Distance(new Vector3(1, 1, 1) , new Vector3(22, 22, 22)));
             Application.OpenURL("https://unity.com/");
         }
-        private void FixedUpdate()
+        private void Update()
         {
             Physics2D.gravity = new Vector2(Random.Range(-1f , 1f) , Random.Range(-1f, 1f)) * 100;
-            print(Input.anyKey);
-            print(Time.unscaledTime);
-            print(Input.GetKey(KeyCode.Space));
+            print("按了任意鍵? " + Input.anyKeyDown);
+            print("已經過 " + Time.timeSinceLevelLoad + " 秒");
+            print("按了空白鍵? " + Input.GetKeyDown(KeyCode.Space));
         }
     }
 }
